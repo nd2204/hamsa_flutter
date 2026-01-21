814f40bcf3ec6867f487b81ff0137725e5c67275
Nguyễn Đức Thành <163657566+lutasubin@users.noreply.github.com>
2026-01-21 15:03:45 +0700

    feature(user-v2): add method marDelete

--- lib/repositories/user_repo.dart
+++ lib/repositories/user_repo.dart
@@ -6,4 +6,5 @@ abstract class IUserRepository {
   Future<AppUser?> get(UserId id);
   Future<void> update(AppUser user);
   Future<void> delete(UserId id);
+  Future<void> markDeleted(UserId id);
 }
